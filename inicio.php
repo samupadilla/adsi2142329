e53f67ddaa838525857c7a1cff9bb01baf2eda7a
srpadilla <srpadilla@misena.edu.co>
2021-08-30 09:16:47 -0500

    explicación de arreglos y funciones

--- inicio.php
+++ inicio.php
@@ -8,7 +8,41 @@
 </head>
 <body>
     <?php 
-    echo 'Comenzamos';
+    echo 'Comenzamos ARREGLOS EN PHP <br>';
+    $barrios= array();
+    $barrios[0]='amistad';
+    $barrios[1]='neruda';
+    $barrios[2]='terreros';
+    $barrios[3]='san mateo';
+    $ciudades=['soacha','sibate','granada'];
+    $numeros=array();
+    $otroarray=[1,2,3,4,5,6,7,8,9];
+    //Funcion Suma de Arreglos
+    
+    function sumaArray($nums){
+        $suma=0;
+        for ($i=0; $i <count($nums) ; $i++) { 
+            $suma=$suma+$nums[$i];
+        }
+        return $suma;
+    }
+
+
+    for ($i=0; $i < 20; $i++) { 
+        $numeros[$i]=rand(0,100);
+    }
+
+    for ($i=0; $i <20 ; $i++) { 
+        echo $numeros[$i].',';
+    }
+    echo '<br>';
+    // $suma=0;
+    // for ($i=0; $i <20 ; $i++) { 
+    //     $suma=$suma+$numeros[$i];
+    // }
+    echo "la suma es: ".sumaArray($numeros).'<br>';
+    echo "la suma es: ".sumaArray($otroarray);
+
     ?>
 </body>
 </html>
